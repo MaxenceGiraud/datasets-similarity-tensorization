Fix covariance computation and eigenvectors in Third Order moment Tensorization

python/tensorize_moments.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.stats import moment
 
 def tensorize_thirdordermoment(V):
-    
-    cov  = np.cov(V.T)
+    Vmu = V - V.mean(axis=0)
+    cov  = (Vmu.T @Vmu )/V.shape[0]
     eig_val,eig_vec = np.linalg.eig(cov)
-    u = eig_vec[eig_val.argmin()]
+    u = eig_vec[:,eig_val.argmin()]
 
     d = (V.T @ (u.T @ (V-V.mean(axis=0)).T)**2) / V.shape[0]
     D = 0 
@@ -13,7 +13,7 @@ def tensorize_thirdordermoment(V):
         ei =np.zeros(V.shape[1])
         ei[i] = 1
         D = D + np.outer(np.outer(d,ei),ei) + np.outer(np.outer(ei,d),ei) + np.outer(np.outer(ei,ei),d)
-    D = D.reshape(np.repeat(V.shape[1],3))
+    D = D.reshape(V.shape[1],V.shape[1],V.shape[1])
     moment3 = moment(V,3,axis=0)
     VV = D - moment3
 